63312bedca6d7ec65965eecb6cd093a0cf2741d2
Ali Hakan Şen <alihakansen4@gmail.com>
2022-12-06 15:50:04 +0300

    Square Prefab Created
    Square prefab created. Every square is gonna carry numbers. Player gonna interact with these squares to play the game.

---
+++ Sudoku/Assets/Scripts/Square/Square.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Square : Selectable
+{
+    [HideInInspector] public int _Rank;
+    [SerializeField] private TextMeshProUGUI _Text;
+    private int _Number = 0;
+    private bool isSolved;
+    private bool created;
+    private void Update()
+    {
+        if (IsPressed())
+            GameActions.instance._PressedAction.Invoke(_Rank);
+    }
+    public void HighlightSquare()
+    {
+        targetGraphic.color = colors.highlightedColor;
+    }
+    public void ResetSquareColor()
+    {
+        targetGraphic.color = colors.normalColor;
+    }
+    public int GetNumber()
+    {
+        return _Number;
+    }
+    public void SetNumber(int number)
+    {
+        _Number = number;
+        created = true;
+        _Text.text = _Number.ToString();
+    }
+    public bool IsCreated()
+    {
+        return created;
+    }
+    public void ResetCreated()
+    {
+        created = false;
+    }
+}
